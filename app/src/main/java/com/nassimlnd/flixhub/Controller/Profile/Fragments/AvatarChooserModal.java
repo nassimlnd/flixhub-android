84906c9fe301638dc38551b6c4a3683a7926d8b5
Nassim LOUNADI <nassimlnd37@gmail.com>
2024-03-29 13:41:28 +0100

    fix(avatar): avatars size in chooser modal

--- app/src/main/java/com/nassimlnd/flixhub/Controller/Profile/Fragments/AvatarChooserModal.java
+++ app/src/main/java/com/nassimlnd/flixhub/Controller/Profile/Fragments/AvatarChooserModal.java
@@ -73,9 +73,11 @@ public class AvatarChooserModal {
             avatarScrollView.addView(avatarList);
             avatarGroup.addView(avatarScrollView);
 
+            int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 75, Resources.getSystem().getDisplayMetrics());
+
             for (String avatar : avatars.get(key)) {
                 ImageView avatarImageView = new ImageView(ctx);
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(150, ViewGroup.LayoutParams.WRAP_CONTENT);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, ViewGroup.LayoutParams.WRAP_CONTENT);
                 layoutParams.setMargins(0, 0, 24, 24);
 
                 avatarImageView.setLayoutParams(layoutParams);
